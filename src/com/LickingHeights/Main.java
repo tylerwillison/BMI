5d90009cc68a39d9ff370cd2b26b92daa44e357b
Tyler <tyler.willison.2022@student.lhschools.org>
2019-09-20 09:38:47 -0400

    made the doubles for the math equations

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,39 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//BMI = kg/m*m
+    //m = inch *(0.0254meters/1inch
+    //inch = m*(1inch/0.0254meter)
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        Double inch;
+        Double pound;
+        int imp;
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
